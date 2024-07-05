703c4f99cf0ac506a0881cde44691e8bfc2f99a0
victorribeirog <vicrgon2010@hotmail.com>
2024-07-05 18:49:26 -0300

    .

---
+++ web-server/src/app.js
@@ -0,0 +1,67 @@
+const path =require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPatch = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPatch))
+
+app.get('',(req, res)=>{
+    res.render('index',{
+        title: 'Weather App',
+        name: 'Victor'
+    })
+})
+
+app.get('/about', (req,res)=>{
+    res.render('about',{
+        title: 'About me',
+        name: 'Victor'
+    })
+})
+
+app.get('/help', (req,res)=>{
+    res.render('help',{
+        title: 'Help Page',
+        name: 'Victor',
+        helptext: 'This is some helpful text.'
+    })
+})
+
+app.get('/weather',(req, res)=>{
+    res.send({
+        forecast: 'Forecast',
+        location: 'Location'
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('error',{
+        title: '404',
+        name: 'Victor',
+        errorText: 'Help article not found..'
+    })
+})
+
+app.get('*',(req, res)=>{
+    res.render('error',{
+        title: '404',
+        name: 'Victor',
+        errorText: 'Page not found.'
+    })
+})
+
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000')
+})
